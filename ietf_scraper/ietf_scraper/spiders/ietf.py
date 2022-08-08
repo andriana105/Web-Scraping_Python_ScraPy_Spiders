33313cfa00d3aea5443bba3bc4e27acc43fe9607
Andriana Butar2 <31553181+andriana105@users.noreply.github.com>
2022-08-08 21:20:31 +0700

    Add files via upload

---
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -0,0 +1,21 @@
+import scrapy
+import w3lib.html
+
+
+class IetfSpider(scrapy.Spider):
+    name = 'ietf'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
+
+    def parse(self, response):
+        return {
+            'number': response.xpath('//span[@class="rfc-no"]/text').get(),
+            'title' : response.xpath('//meta[@name="DC.Title"]/@content').get(),
+            'date': response.xpath('//span[@class="date"]/text()').get(),
+            'description': response.xpath('//meta[@name="DC.Description.Abstract"]/@content').get(),
+            'author': response.xpath('//meta[@name="DC.Creator"]/@content').get(),
+            'company': response.xpath('//span[@class="author-company"]/text()').get(),
+            'address': response.xpath('//span[@class="address"]/text()').get(),
+            'text': w3lib.html.remove_tags(response.xpath('//div[@class="text"]').get()),
+            'headings': response.xpath('//span[@class="subheading"]/text()').getall()
+        }
